7fe68dfb93bc51847436226e33de17cae589e70f
NushiSid <puja.ramani@gmail.com>
2026-02-01 10:50:22 -0800

    Add files via upload

---
+++ Nushi.js
@@ -0,0 +1,7 @@
+let j=()=>{
+   let i=document.getElementById("id").value;
+   document.getElementById("idd").innerHTML="Hi "+i;
+   document.getElementById("idd").style.color="white";
+   document.getElementById("iid").src="https://m.media-amazon.com/images/I/71BnMLLcAsL._AC_UF894,1000_QL80_.jpg";
+}
+document.getElementById("iddd").addEventListener("click",j);
