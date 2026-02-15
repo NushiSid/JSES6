a1112c4503af75db8cbdc7eb3aaa7cb6e5123f52
NushiSid <puja.ramani@gmail.com>
2026-02-15 10:37:39 -0800

    Add files via upload

---
+++ JSES6/Nushi.js
@@ -0,0 +1,21 @@
+let j=()=>{
+   let i=document.getElementById("id").value;
+   document.getElementById("idd").innerHTML="Hi "+i;
+   document.getElementById("idd").style.color="white";
+   document.getElementById("iid").src="https://m.media-amazon.com/images/I/71BnMLLcAsL._AC_UF894,1000_QL80_.jpg";
+}
+document.getElementById("iddd").addEventListener("click",j);
+// let v=()=>{
+//     let va=document.getElementById("iiid")
+//}
+let h=()=>{
+    let i=document.getElementById("id").value;
+    document.getElementById("Id").innerHTML="Hello "+i;
+    document.getElementById("Id").style.color="white";
+    document.getElementById("iD").src="hvd.gif";
+}
+ document.getElementById("iId").addEventListener("click",h);
+ 
+//  let v=()=>{
+//      let va=document.getElementById("iId")
+//  
